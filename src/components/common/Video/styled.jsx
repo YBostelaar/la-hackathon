40e78abc2d7b6db78ecda46986741c0aa981f24a
Ronny <ronny.rook@labela.nl>
2018-12-11 11:18:08 -0800

    fix video

--- src/components/common/Video/styled.jsx
+++ src/components/common/Video/styled.jsx
@@ -3,15 +3,18 @@ import styled, { css } from 'styled-components';
 const Video = styled.video`
 
   ${(props) => props.fullscreen && css`
-    position: absolute;
+    position: absolute !important;
     top: 0;
     right: 0;
     bottom: 0;
     left: 0;
-    z-index: 0;
+    z-index: 0 !important;
     opacity: .8;
     max-width: 100vw;
     max-height: 100vw;
+    height: 100vh;
+    width: 100vw;
+    object-fit: cover;
   `}
 `;
 
