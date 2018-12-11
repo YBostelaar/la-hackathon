2aefff05ca47ebc3f4e7e8cc29e8637e3f30c02a
Yanick Bostelaar <yanick@labela.nl>
2018-12-10 20:42:45 -0800

    add first question

---
+++ src/components/modules/Entry/components/Step2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Title from 'common/Title';
+import Subtitle from 'common/Subtitle';
+
+const Step1 = () => (
+  <>
+    <Title>Good afternoon Jay</Title>
+    <Subtitle>Welcome to your Greenwheels for today</Subtitle>
+  </>
+);
+
+export default Step1;
--- src/components/modules/Entry/index.jsx
+++ src/components/modules/Entry/index.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 
-import ButtonContainer from 'common/ButtonContainer';
-import Button from 'common/Button';
-import Title from 'common/Title';
-import Subtitle from 'common/Subtitle';
+import Step1 from './components/Step1';
+import Step2 from './components/Step2';
+import Step3 from './components/Step3';
 
-const Entry = () => (
-  <>
-    <Title>Goodmorning Jay</Title>
-    <Subtitle green>Welcome to your Greenwheels for today</Subtitle>
-    <ButtonContainer>
-      <Button>LOOKS GOOD</Button>
-      <Button>LOOKS JAAA</Button>
-    </ButtonContainer>
-  </>
-);
+class Entry extends React.Component {
+  state = {
+    step: 1,
+  }
+
+  changeStep = (increment) => {
+    this.setState({ step: this.state.step + increment });
+  }
+
+  render() {
+    switch (this.state.step) {
+      case 1:
+        return <Step1 />;
+      case 2:
+        return <Step2 />;
+      case 3:
+        return <Step3 />;
+      default:
+        return <Step1 />;
+    }
+  }
+}
 
 export default Entry;
